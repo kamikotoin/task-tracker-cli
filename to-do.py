54da64eac23ac02e80a62dbef48205d106f658c7
kamikotoin <166481079+kamikotoin@users.noreply.github.com>
2026-06-18 21:20:23 +0500

    wrong upload
    correcting first previous upload

--- to-do.py
+++ to-do.py
@@ -1,85 +1,76 @@
+import sys
 import json
-
+import os
 
 class TodoList: 
     def __init__(self):
+        if not os.path.exists("tasks.json"):
+                self.tasks = []
+                return
         with open("tasks.json", "r") as f:
             content = f.read()
-            if not content:  # file exists but is empty
+            if not content:
                 self.tasks = []
                 return
             self.tasks = json.loads(content)
 
     def add_task (self, task):
-        self.tasks.append({"task":task, "check":False})
+        self.tasks.append({"task":task, "check":"ToDo"})
+        self.save_tasks()
 
-    def find_task(self, task):
-        if len(self.tasks)==0:
-            print("List is empty, add first")
-            return None
-        for i in range(len(self.tasks)):
-            task = task.lower()
-            if self.tasks[i]["task"].lower() == task:
-                return i
-        return None
-
-    def check_task(self, task):
-        # if len(self.tasks)==0:
-        #     print("List is empty, add first")
-        #     return None
-        i=self.find_task(task)
-        if i == None:
-            print("couldnt find, try again")
-            return None
-        if self.tasks[i]["check"] == False:
-            self.tasks[i]["check"] = True
-        else:
-            self.tasks[i]["check"] = False
+    def update_task(self, task):
+        if task < len(self.tasks) and task >= 0:
+            self.tasks[task]["task"] = sys.argv[3]
+        self.save_tasks()
 
     def delete_task(self, task):
-        # if len(self.tasks)==0:
-        #     print("List is empty, add first")
-        #     return None        
-        i=self.find_task(task)
-        if i != None:
-            self.tasks.pop(i)
+        if task < len(self.tasks) and task >= 0:
+            self.tasks.pop(task)
+            self.save_tasks()
             return None
         print("Sorry, couldnt find such task, maybe try again ")
     
+    def mark_in_progress(self, task):
+        if task < len(self.tasks) and task >= 0:
+            self.tasks[task]["check"] = "In-Progress"
+            self.save_tasks()
+        
+
+    def mark_done(self, task):
+        if task < len(self.tasks) and task >= 0:
+            self.tasks[task]["check"] = "Done"
+            self.save_tasks()
+
+
     def print_tasks(self):
         if len(self.tasks)==0:
             print("List is empty, add first")
-            return None        
-        for i in range(len(self.tasks)):
-            if self.tasks[i]["check"]:
-                print(f"{i+1}.[✅]{self.tasks[i]["task"]}")
-            else:
-                print(f"{i+1}.[ ]{self.tasks[i]["task"]}")
+            return None
+        if len(sys.argv)<3:    
+            for i in range(len(self.tasks)):
+                print(f"{i+1})'{self.tasks[i]["task"]}' is in {self.tasks[i]["check"]} status")
+        else:
+            for i in range(len(self.tasks)):
+                if self.tasks[i]["check"] == sys.argv[2]:
+                    print(f"{i+1})'{self.tasks[i]["task"]}' is in {sys.argv[2]} status")
 
-working = True
-print("Welcome to-do list small code")
+    def save_tasks(self):
+        with open("tasks.json", "w") as f:
+            json.dump(self.tasks, f, indent=2)
+
 my_list = TodoList()
-while working:
-    print("Main menu:\n 1.print list\n 2.add task \n 3.check the task\n 4.delete task\n 0 Exit programm")
-    choice = int(input("Choose option:"))
-    match choice:
-        case 0:
-            working = False
-            with open("tasks.json", "w") as f:
-                json.dump(my_list.tasks, f)
-            print("Good luck! Have fun! Dont die!")
-        case 1:
-            my_list.print_tasks()
-        case 2:
-            choice = input("Enter your task: ")
-            my_list.add_task(choice)
-        case 3:
-            my_list.print_tasks()
-            choice = input("Choose task to check it: ")
-            my_list.check_task(choice)
-        case 4:
-            my_list.print_tasks()
-            choice = input("Choose task to delete it: ")
-            my_list.delete_task(choice)
-        case _:
-            print("\nHah, nice Try\n")
+match sys.argv[1]:
+    case "add":
+        my_list.add_task(sys.argv[2])
+    case "update":
+        my_list.update_task(int(sys.argv[2])-1)
+    case "delete":
+        my_list.delete_task(int(sys.argv[2])-1)
+    case "mark-in-progress":
+        my_list.mark_in_progress(int(sys.argv[2])-1)
+    case "mark-done":
+        my_list.mark_done(int(sys.argv[2])-1)
+    case "list":
+        my_list.print_tasks()
+    case _:
+        print("\nHah, nice Try\n")
